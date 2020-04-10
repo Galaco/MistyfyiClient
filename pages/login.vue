37d829b7def6b1559f903d5931bfa67a4da16b27
Galaco <mail@galaco.me>
2020-04-10 21:22:45 +0900

    Add login page

---
+++ pages/login.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container fill-height>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col cols="4">
+        <v-card>
+          <v-card-title>Ikamu</v-card-title>
+          <v-card-text class="text-center">
+            <v-btn color="primary" @click="login">
+              Login
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'empty',
+  name: 'Login',
+  mounted () {
+    if (this.$route.query.passthrough === 1) {
+      this.login()
+    }
+  },
+  methods: {
+    login () {
+      this.$auth.login()
+    }
+  },
+  head () {
+    return {
+      title: 'Logging you in...'
+    }
+  }
+})
+</script>
